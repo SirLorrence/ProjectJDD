a562ef140816e90e8c9646ca2dcadddfa908765d
Laurence S <33729843+lvlzro@users.noreply.github.com>
2019-11-02 20:26:24 -0300

    PlayerMovement

---
+++ Assets/Player/DetectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectController : MonoBehaviour
+{
+    public PlayerMovement thePlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1)|| Input.GetMouseButton(2)) 
+        {
+            thePlayer.useController = false;
+        }
+
+        if (Input.GetAxisRaw("Mouse X") !=0.0f || Input.GetAxisRaw("Mouse Y") !=0.0f)
+        {
+            thePlayer.useController = false;
+        }
+        if (Input.GetAxisRaw("RHorizontalPS") != 0.0f || Input.GetAxisRaw("RVerticalPS") != 0.0f)
+        {
+            thePlayer.useController = true;
+        }
+    }
+}
---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    float camRayLength = 100f;
+    public bool useController;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+
+
+
+
+    private void Awake()
+    {
+        //get the references
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+    void Turning()
+    {
+        //rotation with mouse
+        if (!useController)
+        {
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit floorHit;
+
+            if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+            {
+                Vector3 playertoMouse = floorHit.point - transform.position;
+                playertoMouse.y = 0f;
+
+                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+                Quaternion newRotation = Quaternion.LookRotation(playertoMouse);
+
+                // Set the player's rotation to this new rotation.
+                playerRigidbody.MoveRotation(newRotation);
+            }
+        }
+
+        //rotation with controller
+        if (useController)
+        {
+
+            //xbox right stick
+            Vector3 playerDirection = Vector3.right * Input.GetAxisRaw("RHorizontalXB") + Vector3.forward * -Input.GetAxisRaw("RVerticalXB");
+            if (playerDirection.sqrMagnitude > 0.0f)
+            {
+                transform.rotation = Quaternion.LookRotation(playerDirection, Vector3.up);
+            }
+            //Playstation right stick
+            Vector3 PlayerDirection = Vector3.right * Input.GetAxisRaw("RHorizontalPS") + Vector3.forward * -Input.GetAxisRaw("RVerticalPS");
+            if (PlayerDirection.sqrMagnitude> 0.0f)
+            {
+                transform.rotation = Quaternion.LookRotation(PlayerDirection, Vector3.up);
+            }
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+
+}
